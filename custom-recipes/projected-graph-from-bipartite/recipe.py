bdbdeff095d0f1367d3277fa6df68df0024fba05
StanislasGuinel <stanislas.guinel@dataiku.com>
2020-08-07 16:09:15 +0200

    fixing multiple CH cards at once [ch52759][ch52653][ch52651][ch52648][ch52647]

--- custom-recipes/projected-graph-from-bipartite/recipe.py
+++ custom-recipes/projected-graph-from-bipartite/recipe.py
@@ -6,6 +6,7 @@ import pandas as pd
 import networkx as nx
 from networkx.algorithms import bipartite
 import logging
+import time
 
 
 # Read recipe config
@@ -25,23 +26,25 @@ columns.append(params[Constants.LINKED_BY])
 
 # Recipe input
 df = input_dataset.get_dataframe(columns=columns)
-logging.info("Bipartite Graph - Dataset loaded...")
+logging.info("Projected graph - Dataset loaded")
 
 # Delete nulls
 df = df[(df[params[Constants.GRAPH_OF]].notnull()) & (df[params[Constants.LINKED_BY]].notnull())]
-logging.info("Bipartite Graph - Removed null values...")
+logging.info("Projected graph - Null values removed")
 
 # Dedup
 dd = df.groupby(columns).size().reset_index().rename(columns={0: 'w'})
-logging.info("Bipartite Graph - Created deduplicated dataset...")
+logging.info("Projected graph - Deduplicated dataset created")
 
-# Creating the bipartite graph
+# Creating the Projected graph
 G = nx.Graph()
 G.add_nodes_from(dd[params[Constants.GRAPH_OF]].unique(),  bipartite=0)
 G.add_nodes_from(dd[params[Constants.LINKED_BY]].unique(), bipartite=1)
 G.add_edges_from(zip(dd[params[Constants.GRAPH_OF]], dd[params[Constants.LINKED_BY]]))
-logging.info("Bipartite Graph - Created bipartite graph...")
+logging.info("Projected graph - NetworkX graph created")
 
+start = time.time()
+logging.info("Projected graph - Creating projected graph...")
 # Projecting the main projected graph
 if params[Constants.WEIGHTED]:
     graph = bipartite.weighted_projected_graph(G, dd[params[Constants.GRAPH_OF]].unique())
@@ -54,9 +57,6 @@ else:
     df = pd.DataFrame(list(graph.edges()))
     df.columns = [params[Constants.GRAPH_OF] + '_1', params[Constants.GRAPH_OF] + '_2']
 
-logging.info("Bipartite Graph - Created projected graph...")
+logging.info("Projected graph - Projected graph computed in {:.4f} seconds".format(time.time()-start))
 
-# Recipe outputs
-logging.info("Bipartite Graph - Writing output dataset...")
-# graph = output_dataset
 output_dataset.write_with_schema(df)
